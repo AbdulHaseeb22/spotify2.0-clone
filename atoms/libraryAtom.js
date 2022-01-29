86b91007cbbaf47c1779f828bf6e2cec0ed4d452
AbdulHaseeb22 <abdulhaseeb.soomro8@gmail.com>
2022-01-29 11:01:50 +0500

    spotify2.0

---
+++ atoms/libraryAtom.js
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+
+export const libraryState = atom({
+    key: "libraryState",
+    default: null,
+})
+
+export const libraryIdState = atom({
+    key: "libraryIdState",
+    default: "37i9dQZF1DX4WYpdgoIcn6",
+})
